7ed4c29cc49e3ce1e77fa88138e2d605fcbe5986
Dompreh <stephenasiedu3654@gmail.com>
2023-01-06 15:04:13 +0000

    first commit

---
+++ components/Author.jsx
@@ -0,0 +1,24 @@
+import Image from "next/image";
+import React from "react";
+
+const Author = ({ author }) => {
+  return (
+    <div className="text-center mt-20 mb-8 p-12 relative rounded-lg bg-black bg-opacity-50">
+      <div className="absolute left-0 right-0 -top-14">
+        <Image
+          unoptimized
+          src={author.photo.url}
+          alt={author.name}
+          width="100"
+          height="100"
+          className="align-middle rounded-full"
+        />
+        
+      </div>
+      <h3 className="text-white  my-4 text-xl font-bold">{author.name}</h3>
+      <p className="text-white text-lg ">{author.bio}</p>
+    </div>
+  );
+};
+
+export default Author;
---
+++ components/Comments.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react";
+import parse from "html-react-parser";
+import { getComments } from "../services";
+import moment from "moment";
+
+const Comments = ({ slug }) => {
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    getComments(slug).then((result) => setComments(result));
+  }, []);
+  return (
+    <>
+      {comments.length > 0 && (
+        <div className="mb-8 shadow-lg rounded-lg bg-white pb-12 p-8">
+          <h3 className="text-xl mb-8 font-semibold border-b pb-4">{comments.lenth}{' '}Comments</h3>
+          {comments.map((comment) => (
+            <div key={comment.createdAt} className='border-gray-200 border-b mb-4 pb-4'>
+              <p className="mb-4">
+                <span className="font-semibold">{comment.name}</span>
+                {' '}
+                on
+                {' '}
+                {moment(comment.createdAt).format('MMM DD, YYYY')}
+              </p>
+              <p className="whitespace-pre-line text-gray-600 w-full">{parse(comment.comment)}</p>
+            </div>
+          ))}
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Comments;
