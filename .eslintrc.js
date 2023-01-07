59571e26774a801a5d497cfe9d2e3b4523da7652
Dompreh <stephenasiedu3654@gmail.com>
2023-01-07 14:22:35 +0000

    entire-build6

--- .eslintrc.js
+++ .eslintrc.js
@@ -55,3 +55,10 @@
 //     ],
 //   },
 // };
+module.exports = {
+    "extends": "next",
+    "rules": {
+      "react/no-unescaped-entities": "off",
+      "@next/next/no-page-custom-font": "off"
+    }
+}
